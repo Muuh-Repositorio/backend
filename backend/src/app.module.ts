33072f99aacd7d12c2134a09fd0c67a26d61ff4e
Cássio Figueiredo <cassio.figueiredof@gmail.com>
2021-08-24 21:54:05 -0300

    Configuração do TypeOrm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './database/typeorm.config';
 
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig)
+  ],
   controllers: [],
   providers: [],
 })
