f827ae2b72a0493ec55963f4541b5aaa05a806e8
Caio Leandro <caioleandro38@gmail.com>
2021-08-31 21:41:25 -0300

    feat : service da busca do usuário pelo cpf

---
+++ backend/src/auth/services/GetUserByCpf.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ServiceCommand } from "src/Interfaces/ServiceCommand";
+import { UserResponse } from "../interfaces/UserResponse";
+import { UserRepository } from "../repository";
+
+@Injectable()
+export class GetUserByCpf implements ServiceCommand{
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository
+    ){}
+
+    async execute(userCpf: string): Promise<UserResponse>{
+        const user = await this.userRepository.findOne({ cpf: userCpf })
+
+        if(!user){
+            throw new NotFoundException('Usuário não encontrado!')
+        }
+
+        return {
+            idt_user: user.idt_user,
+            cpf: user.cpf,
+            name: user.name,
+            email: user.email
+        }
+    }
+}
