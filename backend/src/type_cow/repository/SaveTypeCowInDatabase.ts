c77b27b5ddead996143f83353e7cb463efb22a40
Caio Leandro <caioleandro38@gmail.com>
2021-09-21 21:37:58 -0300

    Repository's de tipo de gado

---
+++ backend/src/type_cow/repository/SaveTypeCowInDatabase.ts
@@ -0,0 +1,30 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { ServiceCommand } from "src/Interfaces/ServiceCommand";
+import { EntityRepository, Repository } from "typeorm";
+import { TypeCowDto } from "../dto/TypeCowDto";
+import { TypeCow } from "../entity/TypeCow.entity";
+
+@EntityRepository(TypeCow)
+export class SaveTypeCowInDatabase extends Repository<TypeCow> implements ServiceCommand{
+
+    async execute(typecowDto: TypeCowDto): Promise<TypeCow>{
+        const { type } = typecowDto
+
+
+        const typeCow = this.create()
+
+        typeCow.type = type
+
+        try {
+            await typeCow.save()
+            return typeCow
+        } catch (error) {
+            if(error.code === '23505'){
+                throw new ConflictException('Tipo já cadastrado!')
+            }else{
+                console.log(error)
+                throw new InternalServerErrorException('Erro no servidor')
+            }
+        }
+    }
+}
