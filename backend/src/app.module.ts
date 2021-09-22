4b96367eeb8f768f9beeb31eadf368eed8d6ca38
Caio Leandro <caioleandro38@gmail.com>
2021-09-21 21:37:20 -0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { typeOrmConfig } from './database/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { FarmModule } from './farm/farm.module';
 import { CowModule } from './cow/cow.module';
+import { TypeCowModule } from './type_cow/TypeCow.module';
 
 @Module({
   imports: [
@@ -12,7 +13,8 @@ import { CowModule } from './cow/cow.module';
     AuthModule,
     FarmModule,
     CowModule,
-    ChildbirthModule
+    ChildbirthModule,
+    TypeCowModule
   ],
   controllers: [],
   providers: [],
