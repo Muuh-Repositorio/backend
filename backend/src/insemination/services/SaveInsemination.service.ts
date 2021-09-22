38a0b8d5589e11ad228eb80562ff8cd0ffc2921d
Luís Felipe Monteiro <felipemonteirose@gmail.com>
2021-09-21 21:19:19 -0300

    Verificação se existe inseminação

--- backend/src/insemination/services/SaveInsemination.service.ts
+++ backend/src/insemination/services/SaveInsemination.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { ServiceCommand } from "src/Interfaces/ServiceCommand";
 import { InseminationDto } from "../dto/InseminationDto";
 import { InseminationResponse } from "../interfaces/InseminationResponse";
+import { InseminationRepository } from "../repository";
 import { SaveInseminationInDatabase } from "../repository/SaveInseminationInDatabase";
 
 @Injectable()
@@ -11,9 +12,15 @@ export class SaveInsemination implements ServiceCommand {
     constructor(
         @InjectRepository(SaveInseminationInDatabase)
         private saveInseminationInDatabase: SaveInseminationInDatabase,
+        private inseminationRepository: InseminationRepository
     ) {}
 
     async execute(inseminationDto: InseminationDto): Promise<InseminationResponse> {
+        const exist = await this.inseminationRepository.findOne({idt_cow: inseminationDto.idt_cow})
+        if(exist) {
+            this.saveInseminationInDatabase.update(exist.idt_insemination, { insemination_date: inseminationDto.insemination_date})
+        }
+
         const insemination = await this.saveInseminationInDatabase.execute(inseminationDto);
         
         return {
