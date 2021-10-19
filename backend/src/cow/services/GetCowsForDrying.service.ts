aa5730b2025bb6da6beb3f66b0be5e14dfa40513
Caio Leandro <caioleandro38@gmail.com>
2021-10-18 22:43:46 -0300

    Update GetCowsForDrying.service.ts

--- backend/src/cow/services/GetCowsForDrying.service.ts
+++ backend/src/cow/services/GetCowsForDrying.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from "@nestjs/common";
-//import { GetChildbirthByCow } from "src/childbirth/services/GetChildbirthByCow.service";
 import { Situations } from "src/cow_situations/Situations.enum";
 import { GetInseminationByCow } from "src/insemination/services";
 import { ServiceCommand } from "src/Interfaces/ServiceCommand";
-//import { DiagnosisDate } from "src/utils/calculations/DiagnosisDate";
 import { DryingDate } from "src/utils/calculations/DryingDate";
 import { GetCowsBySituation } from "./GetCowsBySituation.service";
 
@@ -13,7 +11,6 @@ export class GetCowsForDrying implements ServiceCommand {
         private getCowsBySituations: GetCowsBySituation,
         private dryingDate: DryingDate,
         private getInseminationByCow: GetInseminationByCow,
-        //private diagnosisDate: DiagnosisDate
     ){}
 
     async execute(idt_farm: number): Promise<any[]>{
@@ -21,14 +18,12 @@ export class GetCowsForDrying implements ServiceCommand {
         const pregnantCows = await this.getCowsBySituations.execute(idt_farm, Situations.EMPRENHADA) //TODAS AS VACAS PRENHAS
         
         for(const cow of pregnantCows){
-            //const diagnosis_date = await this.diagnosisDate.calculate(insemination_date) //DATA DE DIAGNÓSTICO DA VACA
-            //const diagnosis_date_millisec = Date.parse(diagnosis_date) //DATA DE DIAGNÓSTICO DA VACA EM MILISSEGUNDOS
-            //const millissec170days = 14688000000 
-            const insemination_date = (await this.getInseminationByCow.execute(cow.idt_cow)).insemination_date //DATA DE INSEMINAÇÃO DA VACA   
+            const insemination_date = (await this.getInseminationByCow.execute(cow.idt_cow)).insemination_date //DATA DE INSEMINAÇÃO DA VACA  
             const today = Date.now() //DATA DO DIA CORRENTE EM MILISSEGUNDOS
-            const drying_date = this.dryingDate.calculate(insemination_date)   
-            const drying_date_millisec = new Date(drying_date).getMilliseconds()
-            
+            const drying_date = this.dryingDate.calculate(insemination_date)  
+            const drying_date_US = this.dryingDate.BRtoUS(drying_date)
+            const drying_date_millisec = new Date(drying_date_US).getMilliseconds()
+
             if(today >= drying_date_millisec){
                 cows.push({
                     idt_farm: cow.idt_farm,
