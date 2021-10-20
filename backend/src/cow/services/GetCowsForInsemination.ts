4e64f45c0d6dff5b94bcacd9baf8fdac6033ded0
Caio Leandro <caioleandro38@gmail.com>
2021-10-20 15:48:35 -0300

    Service de vacas aptas para inseminação

---
+++ backend/src/cow/services/GetCowsForInsemination.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { Situations } from "src/cow_situations/Situations.enum";
+import { ServiceCommand } from "src/Interfaces/ServiceCommand";
+import { AbleDate } from "src/utils/calculations/AbleDate";
+import { GetCowById } from "./GetCowById.service";
+import { GetCowsBySituation } from "./GetCowsBySituation.service";
+
+@Injectable()
+export class GetCowsForInsemination implements ServiceCommand{
+    constructor(
+        private getAbleCows: GetCowsBySituation,
+        private getCowsByID: GetCowById
+    ){}
+
+    async execute(idt_farm: number): Promise<any[]>{
+        const cows = []
+        const ableCows = await this.getAbleCows.execute(idt_farm, Situations.APTA)
+        
+        for(const cow of ableCows){
+            const weight = (await this.getCowsByID.execute(cow.idt_cow)).weight
+            
+            if(ableCows && weight >= 350){
+                cows.push({
+                    idt_farm: cow.idt_farm,
+                    idt_cow: cow.idt_cow,
+                    name: cow.name
+                })
+            }
+        }
+
+        return cows
+
+    }
+}
