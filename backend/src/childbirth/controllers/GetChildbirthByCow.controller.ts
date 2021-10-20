7d80d4ed01b8e6db3242e9770e98647f34d42b6f
Caio Leandro <caioleandro38@gmail.com>
2021-10-20 15:49:41 -0300

    Controller de listagem de partos por vaca

---
+++ backend/src/childbirth/controllers/GetChildbirthByCow.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { ControllerCommand } from "src/Interfaces/ControllerCommand";
+import { ChildbirthResponse } from "../interfaces/ChildbirthResponse";
+import { GetChildbirthByCow } from "../services/GetChildbirthByCow.service";
+
+@ApiTags('Cow')
+@Controller('api/childbirth/:idt_cow')
+export class ChildbirthByCowController implements ControllerCommand{
+    constructor(
+        private getChildbirthByCow: GetChildbirthByCow
+    ){}
+
+    @Get()
+    @ApiOperation({ summary: "Listar uma vaca pelo ID" })
+    @ApiResponse({ status: 200, description: "Vaca retornado com sucesso!"})
+    @ApiResponse({ status: 404, description: "Vaca não encontrada!"})
+    @ApiResponse({ status: 500, description: "Erro no servidor!"})
+    handle(@Param('idt_cow') idt_cow: number): Promise<ChildbirthResponse> {
+        return this.getChildbirthByCow.execute(idt_cow);
+    }
+}
