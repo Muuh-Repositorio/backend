d40071a11c8daacb8b5e5e25aeacddc2a22a500c
Caio Leandro <caioleandro38@gmail.com>
2021-10-30 11:17:50 -0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ChildbirthRepository } from './childbirth/repository/ChildbirthRepository';
-import { GetChildbirthByDate } from './childbirth/services/GetChildbirthByDate.service';
 import { SaveCowSituations } from './cow_situations/SaveCowSituations.service';
-import { Notification } from './utils/mimosa/ChildbirthNotification.service';
+import { Chatbot } from './utils/mimosa/chatbot.service';
+import { Notification } from './utils/mimosa/Notification.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -28,6 +27,9 @@ async function bootstrap() {
   const saveCowSituations = new SaveCowSituations()
   saveCowSituations.execute()
 
+  const initChatbot = new Chatbot(new Notification())
+  initChatbot.execute()
+  
   await app.listen(3000);
 }
 bootstrap();
