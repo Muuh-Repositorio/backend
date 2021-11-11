1d6e5567f9575661553b89917f89f778f9c1dce3
Caio Leandro <caioleandro38@gmail.com>
2021-11-10 21:23:18 -0300

    Update insemination.module.ts

--- backend/src/insemination/insemination.module.ts
+++ backend/src/insemination/insemination.module.ts
@@ -2,25 +2,31 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CowModule } from 'src/cow/cow.module';
 import { SaveInseminationController, GetInseminationByCowController, ListInseminationsController } from './controllers/index';
+import { UpdateInseminationController } from './controllers/UpdateInsemination.controller';
 import { InseminationRepository, SaveInseminationInDatabase } from './repository/index';
+import { UpdateInseminationInDatabase } from './repository/UpdateInsemination';
 import { GetInseminationByCow, SaveInsemination, ListInseminations } from './services/index';
+import { UpdateInsemination } from './services/UpdateInsemination.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       InseminationRepository,
-      SaveInseminationInDatabase
+      SaveInseminationInDatabase,
+      UpdateInseminationInDatabase
     ])
   ],
   controllers: [
     SaveInseminationController,
     GetInseminationByCowController,
-    ListInseminationsController
+    ListInseminationsController,
+    UpdateInseminationController
   ],
   providers: [
     SaveInsemination,
     GetInseminationByCow,
-    ListInseminations
+    ListInseminations,
+    UpdateInsemination
   ],
   exports: [
     GetInseminationByCow
