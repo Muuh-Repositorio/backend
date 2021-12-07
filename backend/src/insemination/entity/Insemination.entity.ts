5f543da833109859fc68cba8ccf5326acdd711bf
Cássio Figueiredo <cassio.figueiredof@gmail.com>
2021-12-06 22:13:31 -0300

    Adição do idt do touro e do semen

--- backend/src/insemination/entity/Insemination.entity.ts
+++ backend/src/insemination/entity/Insemination.entity.ts
@@ -1,5 +1,6 @@
 import { Cow } from "src/cow/entity/Cow.entity" ;
-import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Semen } from "src/semen/entity/Semen.entity";
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Insemination extends BaseEntity {
@@ -10,9 +11,20 @@ export class Insemination extends BaseEntity {
     @JoinColumn({ name: 'idt_cow' })
     idt_cow: number
 
+    @Column({ nullable: null })
+    @ManyToOne(() => Cow, cow => cow.inseminations)
+    @JoinColumn({ name: 'idt_bull' })
+    idt_bull: number
+    
+    @Column({ default: null })
+    @OneToOne(() => Semen, semen => semen.insemination)
+    @JoinColumn({ name: 'idt_semen' })
+    idt_semen: number
+
     @Column({ type: 'date' })
     insemination_date: string
 
     @Column({ default: null })
     diagnosis: boolean
+
 }
